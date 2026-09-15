6e3be55de93d51d465822187714890afc0d41851
Vishwash Chaudhary <vishwaschaudhary456@gmail.com>
2026-09-16 00:20:55 +0530

    Fix Abnormalities

--- Notes.py
+++ Notes.py
@@ -2,14 +2,15 @@ from tkinter import *
 
 BG_COLOR = "#0F172A"
 FG_COLOR = "#F8FAFC"
+ACCENT_COLOR = "#93C5FD"
 
 root = Tk()
 root.title("Notes")
-root.geometry("550x700")
+root.geometry("800x700")
 root.iconbitmap("notes.ico")
 root.config(bg=BG_COLOR)
 
-text_label = Label(root, text="Notes", font=("Cambria", 32), bg=BG_COLOR, fg=FG_COLOR)
-text_label.pack(pady=20)
+title_label = Label(root, text="Notes", font=("Cambria", 32), bg=BG_COLOR, fg=ACCENT_COLOR)
+title_label.pack(pady=20)
 
 root.mainloop()
